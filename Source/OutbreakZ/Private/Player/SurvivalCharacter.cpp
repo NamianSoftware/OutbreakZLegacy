c7d5b801557537c3f556fd3e10219f924324053b
Miroslav <54742822+if-not-skill@users.noreply.github.com>
2023-12-05 23:03:24 +0300

    [player] default input

--- Source/OutbreakZ/Private/Player/SurvivalCharacter.cpp
+++ Source/OutbreakZ/Private/Player/SurvivalCharacter.cpp
@@ -2,18 +2,22 @@
 
 
 #include "Player/SurvivalCharacter.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
 
 #include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
 
 ASurvivalCharacter::ASurvivalCharacter()
 {
 	PrimaryActorTick.bCanEverTick = true;
+	bUseControllerRotationYaw = false;
 
 #pragma region INIT_MESHES
 	HeadMesh = CreateDefaultSubobject<USkeletalMeshComponent>("Head");
-	HeadMesh->bOwnerNoSee = true;
 	SkeletalMeshComponents.Add(HeadMesh);
-	
+	HiddenFirstPersonMeshComponents.Add(HeadMesh);
+
 	BodyMesh = CreateDefaultSubobject<USkeletalMeshComponent>("Body");
 	SkeletalMeshComponents.Add(BodyMesh);
 
@@ -35,16 +39,47 @@ ASurvivalCharacter::ASurvivalCharacter()
 	for (const auto MeshComp : SkeletalMeshComponents)
 	{
 		MeshComp->SetupAttachment(GetMesh());
+		MeshComp->SetLeaderPoseComponent(GetMesh());
 	}
+
+	for(const auto MeshComp : HiddenFirstPersonMeshComponents)
+	{
+		MeshComp->bCastHiddenShadow = true;
+	}
+	
 #pragma endregion
 
+
+#pragma region CAMERA
 	FPCamera = CreateDefaultSubobject<UCameraComponent>("FPCamera");
 	FPCamera->SetupAttachment(HeadMesh, CamaraSocketName);
+
+	TPCameraBoom = CreateDefaultSubobject<USpringArmComponent>("TPCameraBoom");
+	TPCameraBoom->SetupAttachment(GetRootComponent());
+	TPCameraBoom->bEnableCameraLag = true;
+	TPCameraBoom->bEnableCameraRotationLag = true;
+	TPCameraBoom->CameraRotationLagSpeed = 20.f;
+	TPCameraBoom->CameraLagMaxDistance = 30.f;
+
+	TPCamera = CreateDefaultSubobject<UCameraComponent>("TPCamera");
+	TPCamera->SetupAttachment(TPCameraBoom, TPCameraBoom->SocketName);
+#pragma endregion
 }
 
 void ASurvivalCharacter::BeginPlay()
 {
 	Super::BeginPlay();
+
+	SwitchCameraMode(CameraMode);
+
+	if (const APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<
+			UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(DefaultMappingContext, 0);
+		}
+	}
 }
 
 void ASurvivalCharacter::Tick(float DeltaTime)
@@ -55,4 +90,130 @@ void ASurvivalCharacter::Tick(float DeltaTime)
 void ASurvivalCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ASurvivalCharacter::JumpPressed);
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this,
+		                                   &ASurvivalCharacter::JumpReleased);
+
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ASurvivalCharacter::Move);
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ASurvivalCharacter::Look);
+
+		EnhancedInputComponent->BindAction(JogAction, ETriggerEvent::Started, this, &ASurvivalCharacter::JogStarted);
+		EnhancedInputComponent->BindAction(JogAction, ETriggerEvent::Canceled, this, &ASurvivalCharacter::JogFinished);
+		EnhancedInputComponent->BindAction(JogAction, ETriggerEvent::Completed, this, &ASurvivalCharacter::JogFinished);
+
+		EnhancedInputComponent->BindAction(CrouchAction, ETriggerEvent::Started, this,
+		                                   &ASurvivalCharacter::CrouchPressed);
+
+		EnhancedInputComponent->BindAction(SwitchCameraAction, ETriggerEvent::Started, this,
+		                                   &ASurvivalCharacter::ToggleNextCameraMode);
+	}
+}
+
+void ASurvivalCharacter::Move(const FInputActionValue& Value)
+{
+	if (!Controller) return;
+
+	const FVector2D MovementVector = Value.Get<FVector2D>();
+
+	// find out which way is forward
+	const FRotator Rotation = Controller->GetControlRotation();
+	const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+	// get forward vector
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+
+	// get right vector 
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	// add movement 
+	AddMovementInput(ForwardDirection, MovementVector.Y);
+	AddMovementInput(RightDirection, MovementVector.X);
+}
+
+void ASurvivalCharacter::Look(const FInputActionValue& Value)
+{
+	if (!Controller) return;
+
+	const FVector2D LookAxisVector = Value.Get<FVector2D>();
+
+	AddControllerYawInput(LookAxisVector.X);
+	AddControllerPitchInput(LookAxisVector.Y);
+}
+
+void ASurvivalCharacter::JogStarted(const FInputActionValue& Value)
+{
+}
+
+void ASurvivalCharacter::JogFinished(const FInputActionValue& Value)
+{
+}
+
+void ASurvivalCharacter::RunStarted(const FInputActionValue& Value)
+{
+}
+
+void ASurvivalCharacter::RunFinished(const FInputActionValue& Value)
+{
+}
+
+void ASurvivalCharacter::CrouchPressed(const FInputActionValue& Value)
+{
+}
+
+void ASurvivalCharacter::JumpPressed(const FInputActionValue& Value)
+{
+	ACharacter::Jump();
+}
+
+void ASurvivalCharacter::JumpReleased(const FInputActionValue& Value)
+{
+	ACharacter::StopJumping();
+}
+
+void ASurvivalCharacter::ToggleNextCameraMode(const FInputActionValue& Value)
+{
+	const ECameraMode NextCameraMode = static_cast<ECameraMode>((static_cast<uint8>(CameraMode) + 1) % static_cast<
+		uint8>(ECameraMode::ECM_NumCameraModes));
+	SwitchCameraMode(NextCameraMode);
+}
+
+void ASurvivalCharacter::SwitchCameraMode(ECameraMode NewCameraMode)
+{
+	CameraMode = NewCameraMode;
+
+	bool bActiveFPCamera = false;
+	bool bActiveTPCamera = false;
+	bool bHideMeshes = false;
+
+	switch (CameraMode)
+	{
+	case ECameraMode::ECM_FirstPerson:
+		bActiveFPCamera = true;
+		bActiveTPCamera = false;
+		bHideMeshes = true;
+		break;
+
+	case ECameraMode::ECM_ThirdPerson:
+		bActiveFPCamera = false;
+		bActiveTPCamera = true;
+		bHideMeshes = false;
+		break;
+
+	default:
+		break;
+	}
+
+	FPCamera->SetActive(bActiveFPCamera);
+	FPCamera->bUsePawnControlRotation = bActiveFPCamera;
+	
+	TPCamera->SetActive(bActiveTPCamera);
+	TPCameraBoom->bUsePawnControlRotation = bActiveTPCamera;
+
+	for (const auto MeshComp : HiddenFirstPersonMeshComponents)
+	{
+		MeshComp->SetOwnerNoSee(bHideMeshes);
+	}
 }
--- Source/OutbreakZ/Public/Player/SurvivalCharacter.h
+++ Source/OutbreakZ/Public/Player/SurvivalCharacter.h
@@ -3,11 +3,24 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "InputActionValue.h"
 #include "GameFramework/Character.h"
 #include "SurvivalCharacter.generated.h"
 
 
 class UCameraComponent;
+class USpringArmComponent;
+class UInputAction;
+class UInputMappingContext;
+
+
+UENUM(BlueprintType)
+enum class ECameraMode : uint8
+{
+	ECM_FirstPerson UMETA(DisplatName = "FirstPerson"),
+	ECM_ThirdPerson UMETA(DisplatName = "ThirdPerson"),
+	ECM_NumCameraModes UMETA(Hidden)
+};
 
 
 UCLASS()
@@ -23,17 +36,27 @@ protected:
 
 public:
 	virtual void Tick(float DeltaTime) override;
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 #pragma region COMPONENTS
 
-#pragma region Camera
+#pragma region CAMERA
 private:
-	UPROPERTY(EditDefaultsOnly, Category="Components|FirtsPersonCamera")
+	UPROPERTY(EditDefaultsOnly, Category="Components|Camera|FirtsPersonCamera")
 	UCameraComponent* FPCamera;
 
-	UPROPERTY(EditDefaultsOnly, Category="Components|FirtsPersonCamera")
+	UPROPERTY(EditDefaultsOnly, Category="Components|Camera|FirtsPersonCamera")
 	FName CamaraSocketName = "CameraSocket";
+	
+	UPROPERTY(EditDefaultsOnly, Category="Components|Camera|ThirdPersonCamera")
+	USpringArmComponent* TPCameraBoom;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Components|Camera|FirtsPersonCamera")
+	UCameraComponent* TPCamera;
+
+	UPROPERTY(EditDefaultsOnly, Category="Components|Camera")
+	ECameraMode CameraMode = ECameraMode::ECM_FirstPerson;
+
+	void SwitchCameraMode(ECameraMode NewCameraMode);
 #pragma endregion
 
 #pragma region MESHES
@@ -64,4 +87,69 @@ private:
 private:
 	UPROPERTY()
 	TArray<USkeletalMeshComponent*> SkeletalMeshComponents;
+	
+	UPROPERTY()
+	TArray<USkeletalMeshComponent*> HiddenFirstPersonMeshComponents;
+
+	
+#pragma region INPUT
+
+protected:
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+
+private:
+	/** MappingContext */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputMappingContext* DefaultMappingContext;
+
+	/** Jump Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* JumpAction;
+
+	/** Move Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* MoveAction;
+
+	/** Look Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* LookAction;
+
+	/** Jog Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* JogAction;
+
+	/** Crouch Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* CrouchAction;
+
+	/** Jog Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* SwitchCameraAction;
+
+protected:
+	/** Called for movement input */
+	void Move(const FInputActionValue& Value);
+
+	/** Called for looking input */
+	void Look(const FInputActionValue& Value);
+
+	/** Called for jog input */
+	void JogStarted(const FInputActionValue& Value);
+	void JogFinished(const FInputActionValue& Value);
+
+	/** Called for run input */
+	void RunStarted(const FInputActionValue& Value);
+	void RunFinished(const FInputActionValue& Value);
+
+	/** Called for crouch input */
+	void CrouchPressed(const FInputActionValue& Value);
+
+	/** Called for jump input */
+	void JumpPressed(const FInputActionValue& Value);
+	void JumpReleased(const FInputActionValue& Value);
+
+	/** Called for crouch input */
+	void ToggleNextCameraMode(const FInputActionValue& Value);
+
+#pragma endregion
 };
